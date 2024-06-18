f7e27956e058b2e4ca11aed2f3148ec2f0bfd6a2
Bernardus Molkenboer <bart@backdigital.nl>
2024-06-18 11:51:32 +0700

    Fixed something?

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,36 +1,38 @@
 import type { Knex } from "knex";
-import "dotenv/config";
+import dotenv from "dotenv";
 import path from "path";
+
+dotenv.config(); // Load environment variables from .env file
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "mysql2",
     connection: {
-      host: process.env.DATABASE_HOST || "127.0.0.1",
+      host: process.env.DEV_DATABASE_HOST || "127.0.0.1",
       port: parseInt(process.env.DATABASE_PORT || "3306", 10),
-      user: process.env.DATABASE_USER || "root",
-      password: process.env.DATABASE_PASS || "W4chtw00rd!23",
-      database: process.env.DATABASE_DB || "realty_db",
+      user: process.env.DEV_DATABASE_USER || "root",
+      password: process.env.DEV_DATABASE_PASS || "W4chtw00rd!23",
+      database: process.env.DEV_DATABASE_DB || "realty_db",
     },
     pool: { min: 0, max: 10 },
     migrations: {
       tableName: "knex_migrations",
-      directory: path.resolve(__dirname, "migrations"), // Absolute path to the migrations directory
+      directory: path.resolve(__dirname, "src", "migrations"), // Adjust as per your project structure
       extension: "ts", // Use TypeScript extension for development
     },
   },
   production: {
     client: "mysql2",
     connection: {
-      host: process.env.DATABASE_HOST,
+      host: process.env.PROD_DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT || "3306", 10),
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASS,
-      database: process.env.DATABASE_DB,
+      user: process.env.PROD_DATABASE_USER,
+      password: process.env.PROD_DATABASE_PASS,
+      database: process.env.PROD_DATABASE_DB,
     },
     pool: { min: 0, max: 10 },
     migrations: {
-      directory: path.resolve(__dirname, "../dist/migrations"), // Absolute path to the dist migrations directory
+      directory: path.resolve(__dirname, "dist", "migrations"), // Absolute path to the dist migrations directory
       extension: "js", // Use JavaScript extension for production
     },
   },
--- src/migrate.ts
+++ src/migrate.ts
@@ -5,13 +5,15 @@ const environment = process.env.NODE_ENV || "development";
 const config = knexConfig[environment];
 const db = knex(config);
 
-db.migrate
-  .latest()
-  .then(() => {
+async function runMigrations() {
+  try {
+    await db.migrate.latest();
     console.log("Migrations are finished.");
-    return db.destroy();
-  })
-  .catch((err) => {
-    console.error("Error running migrations:", err);
-    return db.destroy();
-  });
+  } catch (error) {
+    console.error("Error running migrations:", error);
+  } finally {
+    await db.destroy();
+  }
+}
+
+runMigrations();
